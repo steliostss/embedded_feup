16e3ba3aafaca497b48030bd68ef47e1ec9596df
alena424 <xtesar36@stud.fit.vutbr.cz>
2020-05-28 11:32:39 +0200

    include schedular

--- src/FreeRTOSv10.3.1/FreeRTOS/Demo/WIN32-MSVC/main_tasks.c
+++ src/FreeRTOSv10.3.1/FreeRTOS/Demo/WIN32-MSVC/main_tasks.c
@@ -91,44 +91,7 @@
 #include "task.h"
 #include "timers.h"
 #include "semphr.h"
-
-/* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
-
-/* The rate at which data is sent to the queue.  The times are converted from
-milliseconds to ticks using the pdMS_TO_TICKS() macro. */
-#define mainTASK_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 200UL )
-#define mainTIMER_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 2000UL )
-
-/* The number of items the queue can hold at once. */
-#define mainQUEUE_LENGTH					( 2 )
-
-/* The values sent to the queue receive task from the queue send task and the
-queue send software timer respectively. */
-#define mainVALUE_SENT_FROM_TASK			( 100UL )
-#define mainVALUE_SENT_FROM_TIMER			( 200UL )
-
-/*-----------------------------------------------------------*/
-
-/*
- * The tasks as described in the comments at the top of this file.
- */
-static void prvQueueReceiveTask(void* pvParameters);
-static void prvQueueSendTask(void* pvParameters);
-
-/*
- * The callback function executed when the software timer expires.
- */
-static void prvQueueSendTimerCallback(TimerHandle_t xTimerHandle);
-
-/*-----------------------------------------------------------*/
-
-/* The queue used by both tasks. */
-static QueueHandle_t xQueue = NULL;
-
-/* A software timer that is started from the tick hook. */
-static TimerHandle_t xTimer = NULL;
+#include "scheduler.h"
 
 /*-----------------------------------------------------------*/
 
