ad8a4cb921402be4b905ef39d943c723fc6707ca
steliostss <tsagarakis.stelios@gmail.com>
2020-06-02 20:34:43 +0300

    Add script to run every test

---
+++ Raspberry/RTOSBench/run_everything.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# if you want round robin, you must edit FreeRtosConfig file and set PREEMPTION to 0
+targets=(
+#	"interrupt_processing"
+#	"event"
+#	"event_processing"
+#	"mq"
+#	"mq_workload"
+#	"mutex"
+#	"mutex_pip"
+#	"mutex_workload"
+#	"mutex_processing"
+#	"sched_latency"
+	"sem"
+	"sem_processing"
+	"sem_prio"
+	"jitter")
+
+cd example/linux/
+
+for i in "${targets[@]}"
+do
+  echo $i
+  sudo ./$i &> $i.txt
+  echo &>> $i.txt
+  echo "-----------------------new execution----------------------" >> $i.txt
+  echo &>> $i.txt
+  sudo ./$i &>> $i.txt
+  echo &>> $i.txt
+  echo "-----------------------new execution----------------------" >> $i.txt
+  echo &>> $i.txt
+  sudo ./$i &>> $i.txt
+  echo "done"
+  echo &>> $i.txt
+done
