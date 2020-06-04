f56dfce2b143c02296c68cb28ad437a01413ee27
Alena Tesarova <xtesar36@stud.fit.vutbr.cz>
2020-06-04 17:05:10 +0200

    csv files and script to generate them

--- Raspberry/RTOSBench/example/freertos-rpi2/kernel_tests/output_script.sh
+++ Raspberry/RTOSBench/example/freertos-rpi2/kernel_tests/output_script.sh
@@ -2,17 +2,17 @@
 
 # if you want round robin, you must edit FreeRtosConfig file and set PREEMPTION to 0ll
 targets=(
-  # "mq"
-  # "mq_workload"
-  # "mq_processing"
-  # "mutex"
-  # "mutex_pip"
-  # "mutex_workload"
-  # "mutex_processing"
-	# "sem"
-	#"sem_processing"
-	"sem_prio"
-  #"round_robin"
+ "mq"
+ "mq_workload"
+ "mq_processing"
+ "mutex"
+ "mutex_pip"
+ "mutex_workload"
+ "mutex_processing"
+ "sem"
+ "sem_processing"
+ "sem_prio"
+ "round_robin"
   )
 
 for i in "${targets[@]}"
@@ -20,7 +20,7 @@ do
     cd $i
     echo "Taking output: "$i
     echo "parsing started"
-    sudo python parser.py $i
+    python ../parser.py $i
     echo "parsing finished"
     echo "-------------------------------------------"
     cd ../
--- Raspberry/RTOSBench/example/freertos-rpi2/kernel_tests/parser.py
+++ Raspberry/RTOSBench/example/freertos-rpi2/kernel_tests/parser.py
@@ -22,20 +22,22 @@ def Diff(li1, li2):
     return (list(set(li2) - set(li1)))
 
 def make_stats(timer_t1, timer_t2, max_ref, min_ref):
-    t1 = numpy.array(timer_t1)
-    t2 = numpy.array(timer_t2)
-    diff = t2 - t1
+    t1 = numpy.abs(numpy.array(timer_t1))
+    t2 = numpy.abs(numpy.array(timer_t2))
+    #print(t1)
+    #print(t2)
+    diff = numpy.abs(t2 - t1)
     avg = numpy.mean(diff)
     f.write( str(max_ref) +"," + str(min_ref) + "," + str(avg) + ","+ str(numpy.median(diff)) + "," + str(max(diff)) + ","+ str(min(diff)) +"\n")
 
 
-with open(filepath,encoding='utf-8', errors='ignore') as fp:
+with open(filepath) as fp:
     for line in fp:
         #print(line)
         if (line.find("max") != -1):
             # end of stats, we need new stats
             max_match = re.search(r'max=(\d*)', line)
-            min_line = fp.readline()
+            min_line = fp.next()
             min_match = re.search(r'min=(\d*)', min_line)
             make_stats(timer_t1,timer_t2,int(max_match.group(1)),int(min_match.group(1)))
             # deleting counter for another run
@@ -43,11 +45,11 @@ with open(filepath,encoding='utf-8', errors='ignore') as fp:
             timer_t2 = []
         # find t1 or t2
         if (line.find(t1) != -1):
-            match = re.search(r't1 (\d*)', line)
+            match = re.search(r't1 (-?\d*)', line)
             timer_t1.append( int(match.group(1))) # matching the number
         else:
             if (line.find(t2) != -1):
-                match = re.search(r't2 (\d*)', line)
+                match = re.search(r't2 (-?\d*)', line)
                 timer_t2.append(int(match.group(1)))  # matching the number
 
 f.close()
