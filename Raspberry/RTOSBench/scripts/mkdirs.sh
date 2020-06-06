bfe994ea62de95ad224e9cd2bc4c7cd29eba15e7
steliostss <tsagarakis.stelios@gmail.com>
2020-06-06 13:19:24 +0300

    mv png and csv

--- Raspberry/RTOSBench/scripts/mkdirs.sh
+++ Raspberry/RTOSBench/scripts/mkdirs.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
 targets=(
-	"interrupt_processing"
-	"event"
-	"event_processing"
 	"mq"
 	"mq_workload"
+	"mq_processing"
 	"mutex"
 	"mutex_pip"
 	"mutex_workload"
 	"mutex_processing"
-	"sched_latency"
 	"sem"
 	"sem_processing"
 	"sem_prio"
+	"sem_workload"
+	# "round_robin"
 	"jitter")
 
-cd ../example/linux/
+# cd ../example/linux/
+cd ../example/freertos-rpi2/kernel_tests
 
 for i in "${targets[@]}"
 do
 #	mv $i executables/
 #	mkdir $i
 #	mv $i.txt $i/
-	mv $i $i"_folder"
+	cd $i
+	mkdir csv
+	mkdir png
+	mv *.csv csv/
+	mv *.png png/
+	cd ..
 done
