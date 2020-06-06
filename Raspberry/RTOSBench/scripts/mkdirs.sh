7962a2d42685099d297aa430aac0a47d13f7561b
steliostss <tsagarakis.stelios@gmail.com>
2020-06-06 13:48:40 +0300

    script to organize csv and png

--- Raspberry/RTOSBench/scripts/mkdirs.sh
+++ Raspberry/RTOSBench/scripts/mkdirs.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
 targets=(
-	"mq"
-	"mq_workload"
+	# "mq"
 	"mq_processing"
+	"mq_workload"
 	"mutex"
 	"mutex_pip"
-	"mutex_workload"
 	"mutex_processing"
+	"mutex_workload"
+	"round_robin"
 	"sem"
-	"sem_processing"
 	"sem_prio"
+	"sem_processing"
 	"sem_workload"
-	# "round_robin"
-	"jitter")
+	# "jitter"
+	)
 
-# cd ../example/linux/
-cd ../example/freertos-rpi2/kernel_tests
+cd ../example/linux/
+# cd ../example/freertos-rpi2/kernel_tests
 
 for i in "${targets[@]}"
 do
 #	mv $i executables/
 #	mkdir $i
 #	mv $i.txt $i/
-	cd $i
+	cd $i"_folder"
+	# cd $i
 	mkdir csv
 	mkdir png
 	mv *.csv csv/
